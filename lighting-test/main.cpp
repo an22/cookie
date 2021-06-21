1dfedac0a4dbf898ae3f848e26165d8ce64ba901
Michael Antiufieiev <mykhailo.antiufieiev@yalantis.net>
2021-06-21 14:40:22 +0300

    Initial Commit

---
+++ lighting-test/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lighting-test
+//
+//  Created by Antiufieiev Michael on 21.06.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
